8da38041c9941e5e5ca7864808f58e7179152072
Sean Mortimer <sean.mortimer@gmail.com>
2020-06-29 17:29:32 -0600

    Learn functions

--- functions.py
+++ functions.py
@@ -1,6 +1,17 @@
-def hello():
-    print('Heeyyy')
+def hello(name):
+    print(f'Heeyyy {name}')
 
 
-hello()
+hello('Sean ' * 5)
 
+# Complete the function by making sure it returns 42. .
+def return_42():
+    # Complete function here
+    return 42  # 'pass' just means "do nothing". Make sure to delete this!
+
+# Create a function below, called my_function, that takes two arguments and returns the result of its two arguments multiplied together.
+
+def my_function(x, y):
+    return x * y
+    
+print(my_function(5, 10))
--- lists.py
+++ lists.py
@@ -20,11 +20,42 @@ print('Sean!')
 
 # Destructring
 person = ('Bob', 42, 'Mechanic')
-name, _, proffession = person
+name1, _, proffession = person
 
-print(name, proffession)
+print(name1, proffession)
 
 ha, *ta = [1, 2, 3, 4, 5]
 
 print(ha)
 print(ta)
+
+# Dictionary comprehensions
+
+# Create a variable called student, with a dictionary.
+# The dictionary must contain three keys: 'name', 'school', and 'grades'.
+# The values for each must be 'Jose', 'Computing', and a tuple with the values 66, 77, and 88.
+student = {'name': 'Jose', 'school': 'Computing', 'grades': (66, 77, 88)}
+
+print(student['school'])
+
+# Assume the argument, data, is a dictionary.
+# Modify the grades variable so it accesses the 'grades' key of the data dictionary.
+def average_grade(data):
+    grades =  data['grades']
+    return sum(grades) / len(grades)
+
+
+# Implement the function below
+# Given a list of students (a list of dictionaries), calculate the average grade received on an exam, 
+# for the entire class
+# You must add all the grades of all the students together
+# You must also count how many grades there are in total in the entire list
+def average_grade_all_students(student_list):
+    total = 0
+    count = 0
+    for student in student_list:
+        total += sum(student['grades'])
+        count += len(student['grades'])
+
+    return total / count
+
