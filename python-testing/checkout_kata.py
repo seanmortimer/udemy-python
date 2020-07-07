ffac6470199592b2074dee69c84a6680c4278551
Sean Mortimer <sean.mortimer@gmail.com>
2020-07-07 14:20:58 -0600

    Testing checkout kata

--- python-testing/checkout_kata.py
+++ python-testing/checkout_kata.py
@@ -1,7 +1,53 @@
 class Checkout:
+    class Discount:
+        def __init__(self, quantity, price):
+            self.quantity = quantity
+            self.price = price
+
+
+    def __init__(self):
+        self.prices = {}
+        self.discounts = {}
+        self.items = {}
+        self.total = 0
     
     def addItemPrice(self, item, price):
-        pass
+        self.prices[item] = price
 
     def addItem(self, item):
-        pass
+        if item not in self.prices:
+            raise Exception("Item is missing price")
+        if item in self.items:
+           self.items[item] += 1
+        else:
+           self.items[item] = 1
+
+    def addDiscount(self, item, quantity, price):
+        discount = self.Discount(quantity, price)
+        self.discounts[item] = discount
+        
+    def totalItems(self):
+        total = 0
+        for (item, cnt) in self.items.items():
+            total += self.calculateItemTotal(item, cnt)
+        return total
+        
+    def calculateItemTotal(self, item, count):
+        total = 0
+        if item in self.discounts:
+            discount = self.discounts[item]
+            if count >= discount.quantity:
+                total += self.calcItemDiscount(item, count, discount)
+            else:        
+                total+= self.prices[item] * count
+        else:        
+            total += self.prices[item] * count
+        return total
+
+    def calcItemDiscount(self, item, count, discount):
+        total = 0
+        quantity = count / discount.quantity
+        total += quantity * discount.price
+        remain = count % discount.quantity
+        total += remain * self.prices[item]
+        return total
--- python-testing/checkout_kata_test.py
+++ python-testing/checkout_kata_test.py
@@ -5,11 +5,30 @@ from checkout_kata import Checkout
 @pytest.fixture()
 def checkout():
     checkout = Checkout()
+    checkout.addItemPrice("a", 1)
+    checkout.addItemPrice("b", 2)
     return checkout
     
 
-def test_canAddItemPrice(checkout):
-    checkout.addItemPrice("a", 1)
-
-def test_canAddItem(checkout):
+def test_totalItems(checkout):
     checkout.addItem("a")
+    assert checkout.totalItems() == 1
+
+def test_total2Items(checkout):
+    checkout.addItem("a")
+    checkout.addItem("b")
+    assert checkout.totalItems() == 3
+
+def test_addDiscount(checkout):
+    checkout.addDiscount("a", 3, 2)
+
+def test_applyDiscount(checkout):
+    checkout.addDiscount("a", 3, 2)
+    checkout.addItem("a")
+    checkout.addItem("a")
+    checkout.addItem("a")
+    assert checkout.totalItems() == 2 
+
+def test_exceptionNoPrice(checkout):
+    with pytest.raises(Exception):
+        checkout.addItem("c")
